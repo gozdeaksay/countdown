30d7566521599a38615359fc44b193556ae61d85
gozdeaksay <gozdeaksay@gmail.com>
2016-10-04 01:38:18 -0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -92,7 +92,7 @@ def post(out):
                 "title": "GET READY!",
                 "text": out,
                 "color": "#7CD197",
-                "image_url": "https://s3.amazonaws.com/gozde-patron/countdown/loading.gif"
+                "image_url": "https://s3.amazonaws.com/gozde-patron/countdown/fireworks.jpg"
             }
         ]
     }
